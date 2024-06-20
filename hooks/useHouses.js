15f3916c4c81f9a23c07f905d08d555c4d1357b7
Brent Fisher <bc.fisher@yahoo.com>
2024-06-19 19:51:24 -0700

    altered api

--- hooks/useHouses.js
+++ hooks/useHouses.js
@@ -13,6 +13,22 @@ const useHouses = () => {
     };
     fetchHouses();
   }, [get]);
+  
+  const postBid = async (bid) => {
+    await fetch(`/api/bids/${bid.houseId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bid),
+    });
+  };
+
+  const addBid = (bid) => {
+    postBid(bid);
+    setBids([...bids, bid]);
+  };
 
   return { houses, setHouses, loadingState };
 };
--- pages/api/houses/index.js
+++ pages/api/houses/index.js
@@ -28,6 +28,7 @@ export default async function handler(req, res) {
 
     case "POST":
       try {
+        return;
         const recordFromBody = req?.body;
         console.log(`RecordFromBody:[${recordFromBody}]`);
         // HouseGateway.save(house);
